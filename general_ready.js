ce5adebc35d0d54119c8943bf076b6dcc7b694e9
cutestowl <keaiifukurou@gmail.com>
2026-05-23 09:43:13 +0800

    Update general_ready.js

--- general_ready.js
+++ general_ready.js
@@ -2,13 +2,13 @@ function general_ready(unit)
 {
     const screen=document.querySelector('#screen');
 
-    const ready=document.createElement('div');
+    const ready=document.createElement('button');
     ready.style.color="rgb(100,100,100)";
     ready.style.position="absolute";
     ready.style.top="50%";
     ready.style.left="50%";
     ready.id="ready";
-    ready.style.fontSize=`${7*unit}px`;
+    ready.style.fontSize=`${5*unit}px`;
     ready.style.transform="translate(-50%,-50%)";
     ready.innerText="READY\nCLICK TO START";
     ready.style.textAlign="center";
